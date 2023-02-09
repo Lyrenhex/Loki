22329d15f0cb8c9f5962f649310ab1f9a3157f9f
Max Heaton <dh64784@gmail.com>
2023-02-09 21:51:32 +0000

    Better error messages

--- src/serenity_handler.rs
+++ src/serenity_handler.rs
@@ -86,14 +86,15 @@ impl EventHandler for SerenityHandler<'_> {
                     let nick = user.nick_in(&ctx.http, guild).await.unwrap_or(user.name);
                     if !nick.starts_with(STREAMING_PREFIX) {
                         // the user is streaming, but they aren't marked as such.
+                        let old_nick = nick.clone();
                         let nick = STREAMING_PREFIX.to_owned()
                             + &nick.chars().take(30).collect::<String>();
                         if let Ok(guild) = guild.to_partial_guild(&ctx.http).await {
                             if let Err(e) = guild
-                                .edit_member(&ctx.http, user.id, |u| u.nickname(nick))
+                                .edit_member(&ctx.http, user.id, |u| u.nickname(nick.clone()))
                                 .await
                             {
-                                error!("{:?}", e);
+                                error!("Nickname update failed: {old_nick} -> {nick}\n{:?}", e);
                             }
                         }
                     }
@@ -106,13 +107,14 @@ impl EventHandler for SerenityHandler<'_> {
                 if let Some(nick) = nick {
                     if nick.starts_with(STREAMING_PREFIX) {
                         // the user isn't streaming any more, but they are still marked as such.
+                        let old_nick = nick.clone();
                         let nick = nick.chars().skip(2).collect::<String>();
                         if let Ok(guild) = guild.to_partial_guild(&ctx.http).await {
                             if let Err(e) = guild
-                                .edit_member(&ctx.http, user.id, |u| u.nickname(nick))
+                                .edit_member(&ctx.http, user.id, |u| u.nickname(nick.clone()))
                                 .await
                             {
-                                error!("{:?}", e);
+                                error!("Nickname update failed: {old_nick} -> {nick}\n{:?}", e);
                             }
                         }
                     }
